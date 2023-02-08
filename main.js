b70f321e2e6f736c2cf12eedea9252ae15b31581
SofiaFesenko <108746952+SofiaFesenko@users.noreply.github.com>
2023-02-08 18:48:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// let a = 10;
+// let b = 20;
+// alert((a + b)*2);
+
+// let a = prompt('enter your name');
+// alert("hi, " + a);
+
+let a = prompt("перша сторона")
+let b = prompt("друга сторона")
+alert("периметр: " + (a * 2 + b * 2) + ", площа: " + (a * b))
+
+let c = Number(prompt("введіть число"))
+alert(c + 10 * 20 - 30 / 5)
